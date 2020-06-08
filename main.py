7bf4d431808388e04473f8b5bd8248fd35034f29
Haydrill <36947890+Haydrill@users.noreply.github.com>
2020-06-07 19:24:21 -0700

    Changed to requests_html library

--- main.py
+++ main.py
@@ -2,51 +2,46 @@
 #USAGE: this program takes in a Magic: the Gathering card name (no punctuation)
 #   from the command line and searches for it on CardKingdom.com
 #   It then returns the first available version of that card
-import webbrowser, requests, sys
-from bs4 import BeautifulSoup
+import sys
+from requests_html import HTMLSession
 
-#input from command line
+# input from command line
 input = ' '.join(sys.argv[1:])
+# input = 'Marchesa, the Black Rose'  # test case
 
 #create url with card name and print it. Filtered by lowest-highest price
 url = 'https://www.cardkingdom.com/catalog/search?filter%5Bipp%5D=20&filter%5Bsort%5D=price_asc&filter%5Bname%5D='
 url = url + input.replace(' ', '+') #the website url uses '+' to join words
 print(url)
+# go to url and make sure it is valid
+session = HTMLSession()
+card_kingdom = session.get(url)
 
-#go to url and make sure it is valid
-res = requests.get(url)
-res.raise_for_status()
+# grab the proper elements for the card's data. then close site
+card_name_and_sets = card_kingdom.html.find('.itemContentWrapper')
+card_amt_and_qty = card_kingdom.html.find('.amtAndPrice')
+card_kingdom.close()
 
-soup = BeautifulSoup(res.text, features='html.parser')
-res.close()
-
-#grab the html of the amount and price of the cards listed
-card_data = soup.find_all('div', {'class': 'itemContentWrapper'})
-price_data = soup.find_all('div', {'class': 'amtAndPrice'})
 print('Searching for card...')
 
-#variable to track current set
+# variable to track current set when looping (intervals of 4)
 set_tracker = 0
 
-#loop through the card_data and amount_data
-for price in price_data :
-    #set amount of each card on each loop
+# loop through each card, check if correct name, and print first one (cheapest)
+for card in card_amt_and_qty :
+    set_tracker //= 4
+    current_card = card_name_and_sets[set_tracker]
 
-    #4 grades per card, so every interval of 4 is the next card
-    card = card_data[int(set_tracker/4)]
+    card_name = current_card.find('.productDetailTitle', first=True).text
+    card_set = current_card.find('.productDetailSet', first=True).text
 
-    #if the amount is not 0, print the price and amount, then exit program
-    if '0' not in str(price.get_text()) :
-        card_name = card.find('span', {'class': 'productDetailTitle'}).get_text()
-        set = card.find('div', {'class': 'productDetailSet'}).get_text()
+    # remove punctation, make uppercase, compare card names, print if same
+    if input.replace(',' , '').upper() in card_name.replace(',' , '').upper() :
+        print('Card found!\n')
+        print(card_name)
+        print(card_set)
+        print(card.text)
+        sys.exit(0)
 
-        #remove commas and then lowercase the input/card name to compare
-        if input.replace(',','').lower() in card_name.replace(',','').lower() :
-            print('Card found!')
-            print(card_name + set.rstrip('\n'))
-            print('\bThere are currently ' + price.get_text().rstrip('\n'))
-            sys.exit(0)
-    set_tracker += 1
-
-#if looped through and all cards have quantity of 0, this is the message
+# if looped through and all cards have quantity of 0, this is the message
 print('Sorry, that card is not currently available on CardKingdom')
