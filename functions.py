25285816b753212a612d1b3691dc60f3419cd6b8
Haydrill <36947890+Haydrill@users.noreply.github.com>
2020-06-07 20:12:44 -0700

    Seperated functions from main, added SCG support
    SCG support is early stages and needs refinement. Also need to update Readme and repo name

---
+++ functions.py
@@ -0,0 +1,96 @@
+#! python3
+#USAGE: this program takes in a Magic: the Gathering card name (no punctuation)
+#   from the command line and searches for it on CardKingdom.com
+#   It then returns the first available version of that card
+
+import sys
+from requests_html import HTMLSession
+from time import sleep
+
+def CardKingdomScrape(input) :
+
+    #create url with card name and print it. Filtered by lowest-highest price
+    url = 'https://www.cardkingdom.com/catalog/search?filter%5Bipp%5D=20&filter%5Bsort%5D=price_asc&filter%5Bname%5D='
+    url = url + input.replace(' ', '+') #the website url uses '+' to join words
+    print(url)
+
+    # go to url and make sure it is valid
+    session = HTMLSession()
+    card_kingdom = session.get(url)
+
+    # grab the proper elements for the card's data. then close site
+    card_name_and_sets = card_kingdom.html.find('.itemContentWrapper')
+    card_amt_and_qty = card_kingdom.html.find('.amtAndPrice')
+    card_kingdom.close()
+
+    print('Searching for card...')
+
+    # variable to track current set when looping (intervals of 4)
+    set_tracker = 0
+    found = False
+
+    # loop through each card, check if correct name, and print first one (cheapest)
+    for card in card_amt_and_qty :
+        set_tracker //= 4
+        current_card = card_name_and_sets[set_tracker]
+
+        card_name = current_card.find('.productDetailTitle', first=True).text
+        card_set = current_card.find('.productDetailSet', first=True).text
+
+        # remove punctation, make uppercase, compare card names, print if amt != 0
+        if input.replace(',' , '').upper() in card_name.replace(',' , '').upper() :
+            if '0' not in card.text :
+                print('Card found!\n')
+                print(card_name)
+                print(card_set)
+                print(card.text)
+                found = True
+
+    # if looped through and all cards have quantity of 0, this is the message
+    if found is False :
+        print('Sorry, that card is not currently available on CardKingdom')
+## END OF CardKingdomScrape
+
+def SCGamesScrape(input) :
+    #create url with card name and print it. Filtered by lowest-highest price
+    url = 'https://starcitygames.com/search.php?search_query='
+    url = url + input
+    print(url)
+
+    #go to url and make sure it is properly rendered with sleep
+    session = HTMLSession()
+    scg = session.get(url)
+    scg.html.render(sleep=3)
+
+    # find every row of the table
+    card_data = scg.html.find('tr')
+    scg.close()
+
+    # loop through the table and find first card with same name and is in stock
+    for card in card_data[1:] :
+        if input in card.text :
+            if 'Qty: Out of Stock' not in card.text :
+                print(card.text)
+                sys.exit(0)
+    print('Sorry, that card was not found on StarCityGames...')
+## END OF SCGamesScrape
+
+# def TCGPlayerScrape(input) :
+#     #create url with card name and print it. Filtered by lowest-highest price
+#     url = 'https://www.tcgplayer.com/search/magic/product?productLineName=magic&q='
+#     url = url + input + '&page=1&productTypeName=Cards'
+#     print(url)
+#
+#     #go to url and make sure it is valid
+#     res = requests.get(url)
+#     res.raise_for_status()
+#
+#     soup = BeautifulSoup(res.text, features='html.parser')
+#     res.close()
+#     print(soup.get_text())
+#
+#     #grab the html of the amount and price of the cards listed
+#     #card_data = soup.find_all('div', {'class': 'search-result__category-name'})
+#     #print(card_data)
+#     #price_data = soup.find_all('div', {'class': 'amtAndPrice'})
+#     #print('Searching for card...')
--- main.py
+++ main.py
@@ -2,47 +2,11 @@
 #USAGE: this program takes in a Magic: the Gathering card name (no punctuation)
 #   from the command line and searches for it on CardKingdom.com
 #   It then returns the first available version of that card
-import sys
-from requests_html import HTMLSession
+import sys, functions
 
-# input from command line
-input = ' '.join(sys.argv[1:])
-# input = 'Marchesa, the Black Rose'  # test case
-
-#create url with card name and print it. Filtered by lowest-highest price
-url = 'https://www.cardkingdom.com/catalog/search?filter%5Bipp%5D=20&filter%5Bsort%5D=price_asc&filter%5Bname%5D='
-url = url + input.replace(' ', '+') #the website url uses '+' to join words
-print(url)
-# go to url and make sure it is valid
-session = HTMLSession()
-card_kingdom = session.get(url)
-
-# grab the proper elements for the card's data. then close site
-card_name_and_sets = card_kingdom.html.find('.itemContentWrapper')
-card_amt_and_qty = card_kingdom.html.find('.amtAndPrice')
-card_kingdom.close()
-
-print('Searching for card...')
-
-# variable to track current set when looping (intervals of 4)
-set_tracker = 0
-
-# loop through each card, check if correct name, and print first one (cheapest)
-for card in card_amt_and_qty :
-    set_tracker //= 4
-    current_card = card_name_and_sets[set_tracker]
-
-    card_name = current_card.find('.productDetailTitle', first=True).text
-    card_set = current_card.find('.productDetailSet', first=True).text
-
-    # remove punctation, make uppercase, compare card names, print if same
-    if input.replace(',' , '').upper() in card_name.replace(',' , '').upper() :
-        if '0' not in card.text :
-            print('Card found!\n')
-            print(card_name)
-            print(card_set)
-            print(card.text)
-            sys.exit(0)
-
-# if looped through and all cards have quantity of 0, this is the message
-print('Sorry, that card is not currently available on CardKingdom')
+#input from command line
+#input = ' '.join(sys.argv[1:])
+input = 'The Ozolith'
+functions.CardKingdomScrape(input)
+functions.SCGamesScrape(input)
+# functions.TCGPlayerScrape(input)
